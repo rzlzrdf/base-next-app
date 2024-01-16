c4ac807ffd641c7e4b2db560329221800f96a9d2
rzlzrdf <rizallazu@student.ub.ac.id>
2024-01-16 10:26:40 +0700

    ...

---
+++ app/auth/login/components/FormLogin.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { Button, Input } from '@nextui-org/react';
+import { Form, Formik } from 'formik';
+import React from 'react';
+
+export const initValuesLoginForm = { email: '', password: '' };
+
+const FormLogin = () => {
+	return (
+		<Formik
+			initialValues={initValuesLoginForm}
+			onSubmit={value => console.log(value)}
+		>
+			{({ values, handleChange, handleSubmit }) => (
+				<Form onSubmit={handleSubmit}>
+					<div className="mt-3">
+						<Input
+							name="email"
+							value={values.email}
+							onChange={handleChange}
+							type="email"
+							variant="faded"
+							label="Email"
+						/>
+					</div>
+					<div className="mt-3">
+						<Input
+							name="password"
+							value={values.password}
+							onChange={handleChange}
+							type="password"
+							variant="faded"
+							label="Password"
+						/>
+					</div>
+					<div className="mt-3">
+						<Button type="submit" className="block w-full">
+							Login
+						</Button>
+					</div>
+				</Form>
+			)}
+		</Formik>
+	);
+};
+
+export default FormLogin;
---
+++ app/auth/login/loading..tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="min-h-screeen w-full bg-slate-700 text-white grid place-content-center">
+			<p className="text-2xl">Loading</p>
+		</div>
+	);
+}
